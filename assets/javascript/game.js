d081291d2693561c4be4637025890918e4c68919
kris mo <kh.mo19@gmail.com>
2017-07-08 21:45:07 -0500

    JS now working for letter search and guess count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,19 +16,90 @@ function randomWordSelector() {
 	var randomWord = hangmanWords[randomArrayIndex];
 	// Deletes word from Array
 	hangmanWords.splice(randomArrayIndex, 1);
-
-	return randomWord
+	return randomWord;
 }
 
 // Outputs Random Word Out To HTML
-function span() {
+function hiddenWord() {
 	var wordLength = randomWord.length;
-	var paragraph = document.getElementById("word");
+	var word = document.getElementById("word");
 
 	for (var i = 0; i < wordLength; i++) {
-		paragraph.innerHTML += "<span class='letters' id='s" + (i+1) + "'>" + "</span>";
+		word.innerHTML += "<span class='span' id='s" + i + "'></span>";
 	}
 }
 
+var wins = 0;
+var loss = 0;
+var score = "";
+var guesses = 8;
 var randomWord = randomWordSelector();
-span();
+
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("loss").innerHTML = loss;
+
+
+hiddenWord();
+	
+// When Key is Pressed
+// document.onkeyup = function letterCheck(event) {
+// 	var counter = 0;
+// 	var lettersUsed = [];
+// 	var guessedLetter = event.key;
+// 	lettersUsed.push(guessedLetter);
+
+// 	for (var y = 0; y < lettersUsed.length; y++) {
+// 		if (guessedLetter === lettersUsed[i])
+// 	}
+
+
+// 	for (var y = 0; y < randomWord.length; y++) {
+// 		if (guessedLetter.toLowerCase() === randomWord.charAt(y)) {
+// 			document.getElementById("s" + y).style.border = 0;
+// 			document.getElementById("s" + y).innerHTML = guessedLetter.toLowerCase();	
+// 		} else {
+// 			counter += 1;
+// 		}
+// 	}
+
+// 	if (counter === randomWord.length) {
+// 		guesses = guesses - 1;
+// 	}
+	
+// }
+
+var lettersUsed = [];
+
+document.onkeyup = function letterCheck(event) {
+	var counter = 0;
+	var hasLetterBeenUsed = false;
+	var guessedLetter = event.key;
+
+	// Checks to see if letter has already been guessed
+	for (var y = 0; y < lettersUsed.length; y++) {
+		if (guessedLetter === lettersUsed[y]) {
+			hasLetterBeenUsed = true;
+		}
+	}
+
+	// Searches for letter in hangman word
+	if (hasLetterBeenUsed === false) {
+		// Add letter to lettersUsed array
+		lettersUsed.push(guessedLetter);
+		for (var y = 0; y < randomWord.length; y++) {
+			// If letter matches, remove blank underline, add letter
+			if (guessedLetter.toLowerCase() === randomWord.charAt(y)) {
+				document.getElementById("s" + y).style.border = 0;
+				document.getElementById("s" + y).innerHTML = guessedLetter.toLowerCase();
+			// If no match with letter, counter should equal length of hangman word
+			} else {
+				counter += 1;
+			}
+		}
+	}
+
+	// Remove a guess if no match
+	if (counter === randomWord.length) {
+		guesses = guesses - 1;
+	}
+}
