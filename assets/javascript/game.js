abebb1bf67b6c15e4bc0593fc78b7611ba9e198c
kris mo <kh.mo19@gmail.com>
2017-07-09 20:32:09 -0500

    finished javascript programming for incorrect guessed letters to display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -40,35 +40,9 @@ document.getElementById("loss").innerHTML = loss;
 
 
 hiddenWord();
-	
-// When Key is Pressed
-// document.onkeyup = function letterCheck(event) {
-// 	var counter = 0;
-// 	var lettersUsed = [];
-// 	var guessedLetter = event.key;
-// 	lettersUsed.push(guessedLetter);
-
-// 	for (var y = 0; y < lettersUsed.length; y++) {
-// 		if (guessedLetter === lettersUsed[i])
-// 	}
-
-
-// 	for (var y = 0; y < randomWord.length; y++) {
-// 		if (guessedLetter.toLowerCase() === randomWord.charAt(y)) {
-// 			document.getElementById("s" + y).style.border = 0;
-// 			document.getElementById("s" + y).innerHTML = guessedLetter.toLowerCase();	
-// 		} else {
-// 			counter += 1;
-// 		}
-// 	}
-
-// 	if (counter === randomWord.length) {
-// 		guesses = guesses - 1;
-// 	}
-	
-// }
 
 var lettersUsed = [];
+var guessedArray = [];
 
 document.onkeyup = function letterCheck(event) {
 	var counter = 0;
@@ -77,7 +51,7 @@ document.onkeyup = function letterCheck(event) {
 
 	// Checks to see if letter has already been guessed
 	for (var y = 0; y < lettersUsed.length; y++) {
-		if (guessedLetter === lettersUsed[y]) {
+		if (guessedLetter.toLowerCase() === lettersUsed[y].toLowerCase() ) {
 			hasLetterBeenUsed = true;
 		}
 	}
@@ -85,7 +59,7 @@ document.onkeyup = function letterCheck(event) {
 	// Searches for letter in hangman word
 	if (hasLetterBeenUsed === false) {
 		// Add letter to lettersUsed array
-		lettersUsed.push(guessedLetter);
+		lettersUsed.push(guessedLetter.toLowerCase() );
 		for (var y = 0; y < randomWord.length; y++) {
 			// If letter matches, remove blank underline, add letter
 			if (guessedLetter.toLowerCase() === randomWord.charAt(y)) {
@@ -102,4 +76,36 @@ document.onkeyup = function letterCheck(event) {
 	if (counter === randomWord.length) {
 		guesses = guesses - 1;
 	}
+
+	updateGuessedLetters();
 }
+
+var unique = [];
+function updateGuessedLetters() {
+guessedLetterFunction();
+div = document.getElementById("lettersGuessed");
+div.innerHTML = " ";
+	for (y = 0; y < unique.length; y++) {
+		div.innerHTML += unique[y].toLowerCase() + " ";
+	}
+}
+
+function guessedLetterFunction() {
+	for (y = 0; y < lettersUsed.length; y++) {
+		var putInArray = true;
+		for (x = 0; x < randomWord.length; x++) {
+			if (lettersUsed[y].toLowerCase() === randomWord.charAt(x).toLowerCase()) {
+				putInArray = false;
+			}
+		}
+		if ( putInArray === true ) {
+			guessedArray.push(lettersUsed[y].toLowerCase());
+		}
+	}
+
+	unique = guessedArray.filter(function(elem, index, self) {
+    return index == self.indexOf(elem);
+	})
+}
+
+
